83fa11468241194804f467d3f7600da9ac660f3c
Wahab Ahmad <w28ahmad@uwaterloo.ca>
2020-09-26 02:46:30 -0400

    Organizing pieces

--- lab1/starter/png_util/catpng.c
+++ lab1/starter/png_util/catpng.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <errno.h>
+#include "zutil.h"
 #include "lab_png.h"
 
-#include <errno.h>
-#include <assert.h>
+#define BUF_LEN (256*32)
 
 extern int errno;
 
-int incr_height(int* height, char* filepath){
+int incr_height(int* height, int* width, char* filepath){
 	/* Open */
 	FILE* fp = fopen(filepath, "rb");
 	if(!fp){
@@ -27,6 +28,7 @@ int incr_height(int* height, char* filepath){
 
 	/* Increment height */
 	*height += get_png_height(&png_IHDR);
+	*width = get_png_width(&png_IHDR);
 
 	/* Close */
 	fclose(fp);
@@ -90,7 +92,6 @@ int create_simple_png(struct simple_PNG* png, char* filepath){
 	length = htonl(png->p_IDAT->length);
 	png->p_IDAT->p_data = malloc(sizeof(U8)*length);
 	fread(png->p_IDAT->p_data, length, sizeof(U8), fp);
-	/* printf("p_data Size: %lu", sizeof(*(png->p_IDAT->p_data))); */
 	fread(&(png->p_IDAT->crc), (size_t)CHUNK_CRC_SIZE, sizeof(U8), fp);
 
 	/* Fill in IEND */
@@ -100,12 +101,25 @@ int create_simple_png(struct simple_PNG* png, char* filepath){
 	
 	length = htonl(png->p_IEND->length);
 	fread(&(png->p_IEND->crc), (size_t)CHUNK_CRC_SIZE, sizeof(U8), fp);
-	read32(&(png->p_IEND->crc), (size_t)CHUNK_CRC_SIZE);
 
 	/* Close */
 	fclose(fp);
 	return 0;
 }
+
+int create_output_file(char* filename, U8* data, int data_len, struct simple_PNG* png, int height, int width){
+	FILE* fp;
+	fp = fopen(filename, "wb+");
+	/* Create size & width (IHDR) data */
+	/* Write IHDR */
+	/* Write IDAT */
+	/* Write IEND */
+	
+	
+	fclose(fp);
+	return 0;
+}
+
 
 int main(int argc, char* argv[]){
 	if(argc == 1){
@@ -114,27 +128,47 @@ int main(int argc, char* argv[]){
 	}
 
 	int height=0;
+	int width=0;
 	int i;
 	for(i=1; i < argc; i++){
-		/* get the total height */
-		incr_height(&height, argv[i]);
+		/* get the total height and width */
+		incr_height(&height, &width, argv[i]);
 	}
 
 	struct simple_PNG png_list[argc-1];
-
 	for(i=1; i < argc; i++){
 		/* simple_PNG of all pics */
 		create_simple_png(&png_list[i-1], argv[i]);
 	}
 
+	int ret = 0;
+	U64 len_inf = 0;
+	int size = height * (width * 4 + 1);
+	int pos = 0;
+	U8 concat_buf[size];
+	/* TODO use the formula to find the size of the buf_inf */
+	U8 buf_inf[BUF_LEN];
 	for(i=1; i < argc; i++){
 		/* Get filtered chunk */
+		ret = mem_inf(buf_inf, &len_inf, png_list[i-1].p_IDAT->p_data, htonl(png_list[i-1].p_IDAT->length));
+		if(ret != 0){
+			fprintf(stderr, "mem inf failed. ret=%d.\n", ret);
+		}
 		/* Concat chunk vertically*/
+		memcpy(&concat_buf[pos], &buf_inf[0], (size_t) len_inf);
+		pos += len_inf;
 	}
 
 	/* Apply compression, generate new IDAT chunk */
+	U8 buf_def[BUF_LEN];
+	U64 len_def = 0;
+	ret = mem_def(buf_def, &len_def, concat_buf, size, Z_DEFAULT_COMPRESSION); 
+	if(ret != 0){
+		fprintf(stderr, "mem def failed. ret=%d.\n", ret);
+	}
 
 	/* Write to ouput file */
+	create_output_file("all.png", buf_def, len_def, &png_list[0], height, width);
 
 	for(i=1; i < argc; i++){
 		/* Free allocated memory */
