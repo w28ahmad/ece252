26ac414644f87ba1eb1e9420035c8ef9301ebf0f
w28ahmad <w28ahmad@uwaterloo.ca>
2020-10-16 00:26:06 -0400

    adding a mutex, removing .out from makefile

--- lab2/starter/e_lab2/paster.c
+++ lab2/starter/e_lab2/paster.c
@@ -9,7 +9,7 @@
 #include "utils.c"
 
 /* Requests image from the server, if the image seq is new save it to a png file */
-int save_image(char* url, bool* seq_images){
+int save_image(char* url, bool* seq_images, pthread_mutex_t* lock){
 	CURL *curl_handle;
 	CURLcode res;
 	RECV_BUF recv_buf;
@@ -17,7 +17,6 @@ int save_image(char* url, bool* seq_images){
 	//pid_t pid =getpid();
 
 	recv_buf_init(&recv_buf, BUF_SIZE);
-
     /* init a curl session */
     curl_handle = curl_easy_init();
 
@@ -47,24 +46,21 @@ int save_image(char* url, bool* seq_images){
 
     if( res != CURLE_OK) {
         fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-    } else {
-		printf("%lu bytes received in memory %p, seq=%d.\n", \
-			recv_buf.size, recv_buf.buf, recv_buf.seq);
+    }
 
-	}		    
+	/* Add a lock to avoid the possibility of 2 threads overwritting the same file */
+	pthread_mutex_lock(lock);
 	/* Save the image if not already saved */
 	if(seq_images[recv_buf.seq] == 0){
     	sprintf(fname, "./output_%d.png", recv_buf.seq);
     	write_file(fname, recv_buf.buf, recv_buf.size);
 		seq_images[recv_buf.seq] = 1;
-	}else{
-		printf("seq %d already exists\n", recv_buf.seq);	
 	}
+	pthread_mutex_unlock(lock);
 
     /* cleaning up */
     curl_easy_cleanup(curl_handle);
-
-    recv_buf_cleanup(&recv_buf);
+	recv_buf_cleanup(&recv_buf);
 	return 0;
 }
 
@@ -74,6 +70,7 @@ struct thread_args              /* thread input parameters struct */
     bool* images;
     int size;
 	char* url;
+	pthread_mutex_t* lock;
 };
 
 
@@ -87,17 +84,17 @@ int array_sum(bool* seq_images, int size){
 
 
 void *thread_function(void* arg){
-	struct thread_args *p_in = arg;
-	int *res = malloc(sizeof(int));
+	struct thread_args *p_in = (struct thread_args*)arg;
+	int *res = (int*)malloc(sizeof(int));
 	while(array_sum(p_in->images, NUM_IMAGES) != NUM_IMAGES){
-		save_image(p_in->url, p_in->images);
+		save_image(p_in->url, p_in->images, p_in->lock);
 	}
 	return ((void *)res);
 }
 
 
 int main(int argc, char** argv){
-	char* url = IMG_URL_1; /* default url */
+	char* url = (char*)IMG_URL_1; /* default url */
 	bool seq_images[NUM_IMAGES] = { 0 };
 
 
@@ -124,10 +121,10 @@ int main(int argc, char** argv){
 				}
 				switch(n){
 					case 2:
-						url = IMG_URL_2;
+						url = (char*)IMG_URL_2;
 						break;
 					case 3:
-						url = IMG_URL_3;
+						url = (char*)IMG_URL_3;
 						break;
 				}
 				break;
@@ -138,14 +135,17 @@ int main(int argc, char** argv){
 
 	/* Create threads to get image segments */
 	curl_global_init(CURL_GLOBAL_DEFAULT);
-	pthread_t *p_tids = malloc(sizeof(pthread_t) * t);
-    struct thread_args in_params;
+	pthread_t *p_tids = (pthread_t*)malloc(sizeof(pthread_t) * t);
+    pthread_mutex_t lock;
+	pthread_mutex_init(&lock, NULL);
+
+	struct thread_args in_params;
     struct thread_ret *p_results[t];
     
 	in_params.images = seq_images;
 	in_params.url = url;
 	in_params.size = t;
-
+	in_params.lock = &lock;
 
     for (int i = 0; i < t; i++) {
         pthread_create(p_tids + i, NULL, thread_function, &in_params);
@@ -153,12 +153,11 @@ int main(int argc, char** argv){
 
     for (int i = 0; i < t; i++) {
         pthread_join(p_tids[i], (void **)&(p_results[i]));
-        printf("Thread ID %lu joined.\n", p_tids[i]);		
+        /* printf("Thread ID %lu joined.\n", p_tids[i]); */
     }
 	curl_global_cleanup();
 
     /* cleaning up */
-
     free(p_tids);
     
     /* the memory was allocated for return values */
@@ -176,9 +175,9 @@ int main(int argc, char** argv){
 		pos += sprintf(&param[pos], "output_%d.png ", i);
 	}
 	
-	char* cmd = concat("./catpng.out ", param);
+	char* cmd = concat("./catpng ", param);
 	system(cmd);
-	
 	free(cmd);
+	
 	return 0;
 }
